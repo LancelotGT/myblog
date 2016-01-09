3393d60880f49e823631a0f8503a2115abfb0d42
Ning Wang <glningwang@gmail.com>
2016-01-09 17:43:25 -0500

    fix and change a few things

--- pelicanconf.py
+++ pelicanconf.py
@@ -39,10 +39,10 @@ PAGE_URL = '{slug}/'
 PAGE_SAVE_AS = PAGE_URL + 'index.html'
 PAGE_PATHS = ['pages']
 
-DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives', 'search']
+DIRECT_TEMPLATES = ['index', 'categories', 'authors', 'archives', 'tags', 'search']
 
 TAG_URL = 'tag/{slug}/'
-TAG_SAVE_AS = TAG_URL
+TAG_SAVE_AS = TAG_URL + "index.html"
 
 CATEGORY_URL = 'category/{slug}/'
 CATEGORY_SAVE_AS = CATEGORY_URL + 'index.html'
@@ -66,7 +66,6 @@ PAGE_LANG_SAVE_AS = None
 
 FEED_RSS = 'feed/index.html'
 FEED_ATOM = 'feed/atom/index.html'
-
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -75,7 +74,7 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 ARTICLE_PATHS = ['posts']
-MENUITEMS = [('posts', '/'), ('cv', '/misc/resume.pdf'), ('categories', '/categories.html'), ('archives', '/archives.html')]
+MENUITEMS = [('posts', '/'), ('cv', '/misc/resume.pdf'), ('categories', '/categories/'), ('archives', '/archives/')]
 ARTICLE_URL = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{date:%d}/{slug}.html'
 
@@ -94,47 +93,36 @@ PLUGINS = [
 TIPUE_SEARCH = True
 
 SITEMAP = {
-        'format': 'xml',
-        'priorities': {
-            'articles': 0.5,
-            'indexes': 0.5,
-            'pages': 0.5,
-            },
-        'changefreqs': {
-            'articles': 'monthly',
-            'indexes': 'daily',
-            'pages': 'monthly',
-            }
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5,
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly',
+    }
 }
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 SOCIAL_WIDGET_NAME = "Contact"
 SOCIAL = (
-            ('@kdeldycke', 'http://twitter.com/kdeldycke'),
+    ('@NingWang92', 'http://twitter.com/NingWang92'),
+    ('Facebook', 'http://facebook.com/wangningLancelot'),
+    ('zhihu', 'https://zhihu.com/people/nwang72')
 )
 
 LINKS_WIDGET_NAME = "Professional profiles"
 LINKS = (
-        ('Careers 2.0', 'http://careers.stackoverflow.com/kdeldycke'),
-        ('LinkedIn', 'http://linkedin.com/in/kevindeldycke/en'),
+    ('LinkedIn', 'http://linkedin.com/in/nwang72'),
+    ('Github', 'https://github.com/LancelotGT'),
+    ('Bitbucket', 'https://bitbucket.org/lancelotGT/')
 )
 
-COPYRIGHT = """Unless contrary mentioned, the licensing terms below applies:
-<br/>
-Code and software released under <a
-href='http://www.fsf.org/licensing/licenses/gpl.html'>GNU/GPL licence v2.0</a>;
-<br/>
-Other content published under <a
-href='http://creativecommons.org/licenses/by-sa/3.0/'>Creative Commons
-Attribution-Share Alike 3.0 license</a>."""
+COPYRIGHT = ""
 
 DISQUS_SITENAME = 'wangnin'
 
-# GOOGLE_ANALYTICS = 'UA-657524-1'
-
-# Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+GOOGLE_ANALYTICS = 'UA-44946494-1'
